6e8abf637ca6a5d527e25e1a718ba629c1e5c6f1
unknown <er.rajeshpunia@gmail.com>
2019-05-05 18:19:56 +0530

    first checkin for pcf project

---
+++ src/main/java/com/pcf/demo/firstpcfapplication/FirstPcfApplication.java
@@ -0,0 +1,13 @@
+package com.pcf.demo.firstpcfapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPcfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcf/demo/firstpcfapplication/FirstPcfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pcf.demo.firstpcfapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstPcfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
